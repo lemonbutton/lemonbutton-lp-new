d4a230e4b9ec92961346ef8df30627986a1cd08f
d4vvv <kacper.godula99@gmail.com>
2022-11-11 17:54:12 +0100

    feat: add responsive design to case studies page

--- components/case-studies/CaseStudyCard.tsx
+++ components/case-studies/CaseStudyCard.tsx
@@ -13,7 +13,7 @@ export const CaseStudyCard: React.FC<CaseStudyCardProps> = ({
   description,
 }) => {
   return (
-    <div className="w-full h-full group bg-lemon relative text-white hover:text-black transition-all duration-300 font-JetBrainsMono  group-hover:border-t-zinc-700 hover:cursor-pointer">
+    <div className="w-full h-full group bg-lemon relative text-white hover:text-black transition-all duration-300 font-JetBrainsMono group-hover:border-t-zinc-700 hover:cursor-pointer min-h-[20rem] ">
       <div className="z-20 absolute p-6 top-1/4 pointer-events-none flex flex-col gap-4">
         <p className="font-medium">{title}</p>
         <div className="border-t-[1px] border-t-white group-hover:border-t-zinc-700 duration-300 transition-all" />
